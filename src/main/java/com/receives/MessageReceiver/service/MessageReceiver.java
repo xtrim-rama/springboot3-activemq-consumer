552482d6b90a63c4d7152c5f67acf1d50aad397a
Durga Barik <durgaltbk@gmail.com>
2023-09-12 20:34:03 +0530

    Manually acknowledge added

--- src/main/java/com/receives/MessageReceiver/service/MessageReceiver.java
+++ src/main/java/com/receives/MessageReceiver/service/MessageReceiver.java
@@ -1,12 +1,33 @@
 package com.receives.MessageReceiver.service;
 
+import jakarta.jms.Message;
+import jakarta.jms.TextMessage;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageReceiver {
     @JmsListener(destination = "learning")
-    public void receiveMessage(String message){
-        System.out.println(message);
+    public void receiveMessage(Message message){
+        try {
+
+            // Simulate processing time with a 5-second delay
+            Thread.sleep(5000);
+            // Manually acknowledge the message
+            message.acknowledge();
+            
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                String messageText = textMessage.getText();
+                System.out.println("Received message: " + messageText);
+            } else {
+                System.out.println("Received message of unsupported type: " + message.getClass().getName());
+            }
+        }
+        catch (Exception e) {
+            // Handle acknowledgment error
+        }
+
+
     }
 }
